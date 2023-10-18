4b3ac9313af51193ed13d70ad1d1fbe2c0cce427
Spyro <124052906+mdspyro@users.noreply.github.com>
2023-10-18 23:46:58 +0400

    newTest added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,29 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+
+    @Test
+    void findMaxWithNegativeNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {-5, -8, -3, -1};
+        long expected = -1;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithEqualIncomes() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {8, 8, 8, 8};
+        long expected = 8;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
